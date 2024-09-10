85c2458d93fef11c85d07d85d7c44c2ba0449f17
Boulahia brahim <98060234+SnowDiles@users.noreply.github.com>
2024-09-10 17:30:42 +0200

    add of function
    display new folder on the sidebar
    function to add the new folder to the form of the task creation 
    creation of the task list 
    creation of new folder

--- index.js
+++ index.js
@@ -1,27 +1,112 @@
-function task(name,description,folder,priorite,date){
- this.name = name;
- this.description = description;
- this.folder = folder;
- this.priorite = priorite;
- this.date = date;
+function task(name, description, folder, priorite, date) {
+  this.name = name;
+  this.description = description;
+  this.folder = folder;
+  this.priorite = priorite;
+  this.date = date;
+}
+
+function folder(name, description) {
+  this.name = name;
+  this.description = description;
 }
 
 let taskLi = [];
 
+function addNewTask(formData, array) {
+  // Crée une nouvelle instance de la classe Task avec les données du formulaire
+  let myTask = new task(
+    formData.get("name"),
+    formData.get("description"),
+    formData.get("folder"),
+    formData.get("priorite"),
+    formData.get("date")
+  );
 
-function addNewTask(name,description,folder,priorite,date,array){
-
-  let myTask = new task(name,description,folder,priorite,date);
-
+  // Ajoute la nouvelle tâche à l'array
   array.push(myTask);
+}
+// @ts-ignore
+document
+  .querySelector("#taskForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const formData = new FormData(this);
+    addNewTask(formData, taskLi);
+    console.log(taskLi);
+  });
+
+// Gestion modal task
+const openModal = document.getElementById("openModal");
+const closeModal = document.getElementById("clodeModal");
+const closeModalSubmit = document.getElementById("clodeModalSubmit");
+const modal_container = document.getElementById("modalTaskContainId");
+openModal?.addEventListener("click", () => {
+  modal_container?.classList.add("show");
+});
+closeModal?.addEventListener("click", () => {
+  modal_container?.classList.remove("show");
+});
+closeModalSubmit?.addEventListener("click", () => {
+  modal_container?.classList.remove("show");
+});
+// gestion modal folder
+const openModal2 = document.getElementById("openModal2");
+const closeModal2 = document.getElementById("clodeModal2");
+const modal_container2 = document.getElementById("modalFolderContainId");
+openModal2?.addEventListener("click", () => {
+  modal_container2?.classList.add("show");
+});
+closeModal2?.addEventListener("click", () => {
+  modal_container2?.classList.remove("show");
+});
+
+let folderArray = [];
+
+// Creation des fonctions folder
+function createFolder(formData, array) {
+  let myFolder = new folder(formData.get("name"), formData.get("description"));
+  array.push(myFolder);
+  addFolderSelect(myFolder);
+  displayListFolder(myFolder);
+}
+
+// @ts-ignore
+document
+  .querySelector("#folderForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const formData = new FormData(this);
+    createFolder(formData, folderArray);
+    console.log(folderArray);
+  });
+
+function addFolderSelect(array) {
+  let folder = document.getElementById("folderSelect");
+  // @ts-ignore
+  folder.insertAdjacentHTML("beforeend", `<option value="${array.name}">${array.name}</option>`);
 }
 
 
-const openModal = document.getElementById("openModal");
-const closeModal = document.getElementById("clodeModal");
-const modal_container = document.getElementById("modalTaskContainId");
+function displayListFolder(array){
+  let folderId = document.getElementById("foldercontain");
+
+  // @ts-ignore
+  folderId.insertAdjacentHTML("beforeend",`<div id="${array.name}"><img src="../Projet_to-do-list/images/icon_dossier.png" alt="image_dossier" srcset=""><p>${array.name}</p></div>`);
+
+}
 
 
-openModal?.addEventListener('click', ()=>{modal_container?.classList.add('show')})
 
-closeModal?.addEventListener('click', ()=>{modal_container?.classList.remove('show')})
+function displayAllTask(array){
+
+  let Idcontent = document.getElementById("content");
+
+  for (let index = 0; index < array.length; index++) {
+    
+    array[index].name
+    
+  }
+
+
+}
