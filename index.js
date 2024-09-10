2a7bef73d6c814ce40b41e36c49d792bfc655937
Boulahia brahim <98060234+SnowDiles@users.noreply.github.com>
2024-09-11 00:33:27 +0200

    add function to display all task, display element with the DOM

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ function addNewTask(formData, array) {
 
   // Ajoute la nouvelle tâche à l'array
   array.push(myTask);
+  displayAllTask(array);
 }
 // @ts-ignore
 document
@@ -100,13 +101,75 @@ function displayListFolder(array){
 
 function displayAllTask(array){
 
-  let Idcontent = document.getElementById("content");
+  let idContent = document.getElementById("taskContain");
+  console.log("mec c'est grave drole");
 
+  // @ts-ignore
+
+  idContent.innerHTML ='';
+  idContent?.insertAdjacentHTML("beforeend", `
+    <div class="flexTaskUp">
+      <div>
+        <form action="#">
+          
+          <p style="display: inline; padding-left: 5px">Ceci est une tache par defaut</p>
+        </form>
+      </div>
+
+      <div style="padding-left: 20px;" class="centralDiv">
+        <img src="images/date_blue.png" alt="date" style="display: inline;"> <p style="display: inline;">L'emplacement de la date</p>
+      </div>
+
+      <div>
+        <img src="images/x_icon.png" alt="" />
+      </div>
+    </div>
+
+    <div class="imgContain">
+      <img src="images/flag_blue.png" alt="" /><p>priorite 1</p>
+      <img src="images/folder_blue.png" alt="" /><p>default</p>
+
+    </div>
+  <hr />`) ;
   for (let index = 0; index < array.length; index++) {
-    
-    array[index].name
-    
+    console.log("me voici dans la boucle");
+
+    // @ts-ignore
+    idContent?.insertAdjacentHTML("beforeend", `
+          <div class="flexTaskUp">
+            <div>
+              <form action="#">
+                <input type="checkbox" name="${array[index].name}" id="${array[index].name}" value="test" />
+                <p style="display: inline; padding-left: 5px">${array[index].name}</p>
+              </form>
+            </div>
+
+            <div style="padding-left: 20px;" class="centralDiv">
+              <img src="images/date_blue.png" alt="date" style="display: inline;"> <p style="display: inline;">${array[index].date}</p>
+            </div>
+
+            <div>
+              <img src="images/x_icon.png" alt="" onclick="deleteTask('${array[index].name}')"/>
+            </div>
+          </div>
+
+          <div class="imgContain">
+            <img src="images/flag_blue.png" alt="" /><p>${array[index].priorite}</p>
+            <img src="images/folder_blue.png" alt="" /><p>${array[index].folder}</p>
+
+          </div>
+        <hr />`) ;
+        console.log(idContent);
+    console.log(array[index].priorite);
   }
+
+  console.log("je suis apres la boucle");
+}
+
+
+function deleteTask(){
+
+    console.log("tesssst");
 
 
 }
